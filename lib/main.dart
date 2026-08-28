a5440503cd46db2aa71059a2c9d3e635cb15ca65
Vorxy <Vlad-imir.Dallakyan@yandex.ru>
2026-08-28 21:06:18 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'screens/home_screen.dart';
 import 'screens/settings_screen.dart';
 import 'screens/editor_screen.dart';
@@ -11,44 +10,18 @@ void main() {
   runApp(const VorxyCodeEditor());
 }
 
-class VorxyCodeEditor extends StatefulWidget {
+class VorxyCodeEditor extends StatelessWidget {
   const VorxyCodeEditor({super.key});
-
-  @override
-  State<VorxyCodeEditor> createState() => _VorxyCodeEditorState();
-}
-
-class _VorxyCodeEditorState extends State<VorxyCodeEditor> {
-  bool _isDarkMode = true;
-
-  @override
-  void initState() {
-    super.initState();
-    _loadTheme();
-  }
-
-  Future<void> _loadTheme() async {
-    final prefs = await SharedPreferences.getInstance();
-    setState(() {
-      _isDarkMode = prefs.getBool('darkMode') ?? true;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Vorxy Code Editor',
-      theme: _isDarkMode ? AppTheme.darkTheme : AppTheme.lightTheme,
-      home: HomeScreen(
-        onThemeChanged: (isDark) {
-          setState(() {
-            _isDarkMode = isDark;
-          });
-        },
-      ),
+      theme: AppTheme.darkTheme,
+      home: const HomeScreen(),
       debugShowCheckedModeBanner: false,
       routes: {
-        '/settings': (context) => const SettingsScreen(currentLanguage: 'ru', onThemeChanged: null),
+        '/settings': (context) => const SettingsScreen(currentLanguage: 'ru'),
         '/editor': (context) => const EditorScreen(currentLanguage: 'ru'),
         '/files': (context) => const FilesScreen(currentLanguage: 'ru'),
         '/languages': (context) => const LanguagesScreen(currentLanguage: 'ru'),
