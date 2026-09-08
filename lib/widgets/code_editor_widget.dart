2f6fb5f5ce74028dd06f58913fac3d22a6ecb77c
Vorxy <Vlad-imir.Dallakyan@yandex.ru>
2026-09-08 13:20:53 +0300

    Update code_editor_widget.dart

--- lib/widgets/code_editor_widget.dart
+++ lib/widgets/code_editor_widget.dart
@@ -22,7 +22,7 @@ class CodeEditorWidget extends StatefulWidget {
 }
 
 class _CodeEditorWidgetState extends State<CodeEditorWidget> {
-  late final CodeController _controller;
+  late TextEditingController _controller;
   final FocusNode _focusNode = FocusNode();
   String _displayCode = '';
   int _cursorPosition = 0;
@@ -35,7 +35,7 @@ class _CodeEditorWidgetState extends State<CodeEditorWidget> {
   @override
   void initState() {
     super.initState();
-    _controller = CodeController(text: widget.code);
+    _controller = TextEditingController(text: widget.code);
     _displayCode = widget.code;
     _controller.addListener(() {
       setState(() {
